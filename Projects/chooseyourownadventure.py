a06643b9690e195399d4888fa2edd2a277500b19
domer-from-agartha61 <alexwall@seattleacademy.org>
2025-12-11 19:55:08 +0000

    hi josh

---
+++ Projects/chooseyourownadventure.py
@@ -0,0 +1,7 @@
+answer1=input("since school is over do you wanna go watch the basketball game")
+if answer1 == "yes":
+   print("then you driving me cuh")
+   if answer1 == "no":
+    print ("Alright then i'll go by myself")
+    
+    
